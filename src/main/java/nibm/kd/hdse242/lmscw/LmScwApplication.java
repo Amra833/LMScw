12e59bfe131e11020383b9d8eb3487e38b1eb48b
Amra833 <amraazwer20@gmail.com>
2025-01-26 11:10:00 +0900

    Initial commit

---
+++ src/main/java/nibm/kd/hdse242/lmscw/LmScwApplication.java
@@ -0,0 +1,13 @@
+package nibm.kd.hdse242.lmscw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmScwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmScwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nibm/kd/hdse242/lmscw/LmScwApplicationTests.java
@@ -0,0 +1,13 @@
+package nibm.kd.hdse242.lmscw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmScwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
